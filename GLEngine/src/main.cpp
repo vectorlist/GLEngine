6f9fe41f3a943eda00023ac35ec2d663c3d7fc17
vectorlist <xarchx80@gmail.com>
2017-04-22 20:00:59 +0900

    updated
    updated

--- GLEngine/src/main.cpp
+++ GLEngine/src/main.cpp
@@ -16,27 +16,6 @@ void setConsoleOutput(int x, int y, int w, int h)
 	MoveWindow(console, x, y, w, h, TRUE);
 }
 
-class SomeData
-{
-public:
-	SomeData(const char* d) : data(d){}
-	bool operator==(char* otherData)
-	{
-		return (this->data == otherData);
-	}
-
-	//this friend dose not meaning just for declare u can use it without this
-	//but u need it if we can find this func in cpp or somewhere
-	//it can't be const func it's already const func
-	friend bool operator==(const char* firstinput,const SomeData &secondinput)
-	{
-		return (firstinput == secondinput.data);
-	}
-	
-	const char* data;
-};
-
-
 int main(int args, char* argv[])
 {
 	setConsoleOutput(50, 50, 600, 720);
@@ -44,7 +23,6 @@ int main(int args, char* argv[])
 	Application app("OpenGL Engine",1280,720);
 	Renderer renderer;
 	
-
 	renderer.setRenderMode(RENDER_FORWARD);
 	Initializer::initCamera(renderer);
 	Initializer::initScene(renderer);
@@ -54,14 +32,7 @@ int main(int args, char* argv[])
 	EnvironmentRenderer environmentRenderer;
 
 	app.run(renderer, terrainRenderer, environmentRenderer);
-	
-	
-	/*for(int i =0; i < 10; ++i)
-		LOG << selectiveBuRandom() << ENDL;;
-
-
-	system("pause");*/
-
+	//system("pause");
 	return 0;
 }
 
